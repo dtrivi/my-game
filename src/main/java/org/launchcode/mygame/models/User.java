5a0683d75e920553fd6a04d82abddde3c1a9d04b
dtrivi <danieltrivinos@gmail.com>
2021-05-02 17:44:13 -0500

    Added MySQL dependency to pom.xml and created a User class to initiate registration/login feature.

--- src/main/java/org/launchcode/mygame/models/User.java
+++ src/main/java/org/launchcode/mygame/models/User.java
@@ -1,4 +1,27 @@
 package org.launchcode.mygame.models;
 
-public class User {
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends AbstractEntity {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
 }
